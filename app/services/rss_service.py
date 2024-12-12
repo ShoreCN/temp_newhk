ba9fcbfcbf21d7fff87fcf4cd7c12c56b04f5ffa
Shore <c.shore.cn@gmail.com>
2024-12-12 18:50:17 +0800

    perf: use information topic as title

--- app/services/rss_service.py
+++ app/services/rss_service.py
@@ -79,7 +79,7 @@ class RSSService:
                 content_type=feed.content_type,
                 category=feed.category,
                 topic=feed.topic,
-                title=feed_data.get("feed", {}).get("title", ""),   # 使用数据源的标题
+                title=feed.topic,   # 使用数据源的topic标题
                 sub_title=feed_data.get("feed", {}).get("title", ""),  # 使用数据源的子标题
                 original_data_path=feed.full_path if feed.full_path else feed.relative_path,
                 source_list=[source],
